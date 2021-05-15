3eefbaa102d2a44e2a55d02188fd4f86f4ee3f89
Aymeric <aymericbertrand.pro@gmail.com>
2021-05-15 23:52:16 +0200

    add modal script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
 function init() {
-    
+    let myLibrary = [];
+
+    function Book() {
+        // the constructor...
+    }
+
+    function addBookToLibrary() {
+        // do stuff here
+    }
+
+    // modal
+    function displayModal() {
+        const modal = document.querySelector(".book__modal");
+        const btn = document.querySelector(`.body__icon--add > svg`);
+        const span = document.querySelector(".modal__close");
+
+        btn.addEventListener('click', () => {
+            modal.classList.remove('hidden');
+            bodyScrollLock.disableBodyScroll(modal);
+        });
+
+        span.addEventListener('click', () => {
+            modal.classList.add('hidden');
+            bodyScrollLock.enableBodyScroll(modal);
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target == modal) {
+                modal.classList.add('hidden');
+                bodyScrollLock.enableBodyScroll(modal);
+            }
+        });
+    }
+    displayModal()
 }
+init();
