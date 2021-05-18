c2d701ee0df9368e3a71cdbfb262fbba98182300
Aymeric <aymericbertrand.pro@gmail.com>
2021-05-18 20:53:36 +0200

    Store user book in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,55 @@
 function init() {
+    const modal = document.querySelector(".book__modal");
+
     let myLibrary = [];
     const pageTitle = document.querySelector('.container__header h1');
-    pageTitle.addEventListener('click', () => console.log(myLibrary));
-        class Book {
-            constructor(title, author, nbOfpages, read) {
-                this.title = title;
-                this.author = author;
-                this.nbOfpages = nbOfpages;
-                this.read = read;
-            }
+    pageTitle.addEventListener('click', () => {
+        console.log(myLibrary);
+    });
+
+    class Book {
+        constructor(title, author, nbOfpages, read) {
+            this.title = title;
+            this.author = author;
+            this.nbOfpages = nbOfpages;
+            this.read = read;
         }
+    }
 
-    function addBookToLibrary() {
-        const addButton = document.querySelector(".button__addButton");
-        const title = document.getElementById("row__title").value;
-        const author = document.getElementById("row__author").value;
-        const nbOfPages = document.getElementById("row__pages").value;
-        const read = document.getElementById("row__read");
+    function submitForm() {
+        const submit = document.querySelector(".button__addButton");
 
-        addButton.addEventListener('click', () => {
-            myLibrary.push(new Book(title, author, nbOfPages, read))
-            console.log(myLibrary);
+        submit.addEventListener('click', () => {
+            const title = document.getElementById("row__title").value;
+            const author = document.getElementById("row__author").value;
+            const nbOfPages = document.getElementById("row__pages").value;
+            const read = document.getElementById("row__read").checked;
+
+            if (title !== "" && author !== "" && nbOfPages !== "") {
+                addBookToLibrary(title, author, nbOfPages, read);
+                hideModal();
+            } else {
+                return alert("Please fill the inputs");
+            }
         });
     }
-    addBookToLibrary();
+
+    function addBookToLibrary(title, author, nbOfPages, read) {
+        myLibrary.push(new Book(title, author, nbOfPages, read));
+    }
 
     // modal
     function displayModal() {
-        const modal = document.querySelector(".book__modal");
-        const btn = document.querySelector(`.library__book--add`);
+        const openModal = document.querySelector(`.library__book--add`);
         const span = document.querySelector(".header__close");
-        const form = document.querySelectorAll(".content__body input")
 
-        function hideModal() {
-            modal.classList.add('hidden');
-            bodyScrollLock.enableBodyScroll(modal);
-            form.forEach(e => e.value = '');
-        }
-
-        btn.addEventListener('click', () => {
+        openModal.addEventListener('click', () => {
             modal.classList.remove('hidden');
             bodyScrollLock.disableBodyScroll(modal);
         });
 
         span.addEventListener('click', () => {
-            return hideModal();
+            hideModal();
         });
 
         window.addEventListener('click', (event) => {
@@ -53,6 +58,15 @@ function init() {
             }
         });
     }
-    displayModal()
+
+    function hideModal() {
+        const form = document.querySelectorAll(".content__body input")
+        modal.classList.add('hidden');
+        bodyScrollLock.enableBodyScroll(modal);
+        form.forEach(e => e.value = '');
+    }
+
+    displayModal();
+    submitForm();
 }
 init();
